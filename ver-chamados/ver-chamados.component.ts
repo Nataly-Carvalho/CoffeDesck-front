60a9d98a423c5d0e6ab55bf6418c0423a8ef4551
Bianca Leandro Barreto <72078345+BiancalBarreto@users.noreply.github.com>
2024-05-14 18:30:27 -0300

    Add files via upload

---
+++ ver-chamados/ver-chamados.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ver-chamados',
+  templateUrl: './ver-chamados.component.html',
+  styleUrls: ['./ver-chamados.component.css']
+})
+export class VerChamadosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
